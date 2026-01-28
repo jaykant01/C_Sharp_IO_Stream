f6ce5ebff10bd22821f1311336907ae536acfa01
jaykant01 <jaykanty24092004@gmail.com>
2026-01-28 12:30:06 +0530

    [Jaykant] Add .BufferedStream 100mb file copy and performance

---
+++ Streams/LargeFileCopy.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Diagnostics;
+namespace Streams;
+
+public class LargeFileCopy
+{
+    private const int BufferSize = 4096;
+
+    public void CopyFile(string source, string destin)
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        
+
+        using (FileStream fsRead = new FileStream(source, FileMode.Open, FileAccess.Read))
+        using (FileStream fsWrite = new FileStream(destin, FileMode.Create, FileAccess.Write))
+        using (BufferedStream bsRead = new BufferedStream(fsRead, BufferSize))
+        using (BufferedStream bsWrite = new BufferedStream(fsWrite, BufferSize))
+        {
+            byte[] buffer = new byte[BufferSize];
+            int bytesRead;
+
+            while((bytesRead = bsRead.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                bsWrite.Write(buffer, 0, bytesRead);
+            }
+        }
+        stopwatch.Stop();
+        Console.WriteLine($"BufferedStream Time: {stopwatch.ElapsedMilliseconds} ms");
+    }
+}
--- Streams/Program.cs
+++ Streams/Program.cs
@@ -4,10 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        string sourcePath = @"D:\\C# All Repo\\C_Sharp_IO_Stream\\Streams\\input.txt";
-        string destinationPath = @"D:\\C# All Repo\\C_Sharp_IO_Stream\\Streams\\output.txt";
-        FileCopyService fileCopy = new FileCopyService();
-        fileCopy.CopyFile(sourcePath,destinationPath);
+        // File Copy 
+        //string sourcePath = @"D:\\C# All Repo\\C_Sharp_IO_Stream\\Streams\\input.txt";
+        //string destinationPath = @"D:\\C# All Repo\\C_Sharp_IO_Stream\\Streams\\output.txt";
+        //FileCopyService fileCopy = new FileCopyService();
+        //fileCopy.CopyFile(sourcePath,destinationPath);
+
+        // 100mb File Copy
+        string source = @"D:\C# All Repo\C_Sharp_IO_Stream\Streams\100mb.txt";
+        string destin = @"D:\C# All Repo\C_Sharp_IO_Stream\Streams\100mbOutput.txt";
+
+        LargeFileCopy largefile = new LargeFileCopy();
+        largefile.CopyFile(source,destin);
 
         Console.ReadKey();
     }
